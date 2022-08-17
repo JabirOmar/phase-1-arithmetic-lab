468df7c41d35463bab80f30ee47b1b1fe85e937f
Jabir Omar <jabiromar400@gmail.com>
2022-08-17 06:42:19 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+
+// First Test
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+//Second Test
+let a = 1;
+let random = a++;
+
+//Third Test
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+//Fourth Test
+let max = (5, 10, 15, 20);
+Math.max=(max)
+
+
+
+
